e71df9c6228e61d546eb358b9a0eedf355304ece
rootxed <57708765+rootxed@users.noreply.github.com>
2022-12-16 20:19:18 +0100

    Initial commit

---
+++ src/main/java/be/atc/managedBean/UserBean.java
@@ -0,0 +1,11 @@
+package be.atc.managedBean;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+
+public class UserBean implements Serializable {
+}
